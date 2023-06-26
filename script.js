ff80e6af8716e5b6ee932b8e172592f782711e14
TusharRB <91899841+TusharRB@users.noreply.github.com>
2023-06-26 12:54:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,6 +5,23 @@ const person = {
   age: 80,
 };
 
+const players = ["John", "Mike", "Sarah"];
+const person = { name: "Alice", age: 25 };
+
+// Create a reference to the players array
+const team = players;
+
+// Create a copy of the players array
+const team1 = [...players];
+
+// Create a copy of the person object
+const cap1 = { ...person };
+
+// Testing the variables
+console.log(team);   // Output: ["John", "Mike", "Sarah"]
+console.log(team1);  // Output: ["John", "Mike", "Sarah"]
+console.log(cap1);   // Output: { name: "Alice", age: 25 }
+
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 
 window.players = players;
